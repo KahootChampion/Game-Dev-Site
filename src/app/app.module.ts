ff8e397dbca0d3b6bfd1d0dcf1d615bd3954746f
Haris Muhammad <harismuhammad@Hariss-MacBook-Pro.local>
2023-05-01 07:38:59 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OverlayComponent } from './overlay/overlay.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverlayComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
